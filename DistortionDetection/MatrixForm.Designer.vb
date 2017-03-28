d71634902a53d1c184a0a8fd13bcc5e08d0c37f8
CatherineStr <strogonova-k@yandex.ru>
2017-03-28 23:46:39 +0300

    Матричный фильтр протестирован, добавлена форма для задания матрицы.

---
+++ DistortionDetection/MatrixForm.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MatrixForm
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.matrixDataGrid = New System.Windows.Forms.DataGridView()
+        Me.apply_btn = New System.Windows.Forms.Button()
+        Me.cancel_btn = New System.Windows.Forms.Button()
+        CType(Me.matrixDataGrid, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'matrixDataGrid
+        '
+        Me.matrixDataGrid.AllowUserToAddRows = False
+        Me.matrixDataGrid.AllowUserToDeleteRows = False
+        Me.matrixDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.matrixDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.matrixDataGrid.ColumnHeadersVisible = False
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle1.Format = "N3"
+        DataGridViewCellStyle1.NullValue = "0"
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.matrixDataGrid.DefaultCellStyle = DataGridViewCellStyle1
+        Me.matrixDataGrid.Location = New System.Drawing.Point(13, 13)
+        Me.matrixDataGrid.Name = "matrixDataGrid"
+        Me.matrixDataGrid.RowHeadersVisible = False
+        Me.matrixDataGrid.RowTemplate.DefaultCellStyle.Format = "N3"
+        Me.matrixDataGrid.RowTemplate.DefaultCellStyle.NullValue = "0"
+        Me.matrixDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect
+        Me.matrixDataGrid.Size = New System.Drawing.Size(259, 237)
+        Me.matrixDataGrid.TabIndex = 0
+        '
+        'apply_btn
+        '
+        Me.apply_btn.Location = New System.Drawing.Point(13, 256)
+        Me.apply_btn.Name = "apply_btn"
+        Me.apply_btn.Size = New System.Drawing.Size(123, 29)
+        Me.apply_btn.TabIndex = 1
+        Me.apply_btn.Text = "Применить"
+        Me.apply_btn.UseVisualStyleBackColor = True
+        '
+        'cancel_btn
+        '
+        Me.cancel_btn.Location = New System.Drawing.Point(149, 256)
+        Me.cancel_btn.Name = "cancel_btn"
+        Me.cancel_btn.Size = New System.Drawing.Size(123, 29)
+        Me.cancel_btn.TabIndex = 2
+        Me.cancel_btn.Text = "Отмена"
+        Me.cancel_btn.UseVisualStyleBackColor = True
+        '
+        'MatrixForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 288)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.cancel_btn)
+        Me.Controls.Add(Me.apply_btn)
+        Me.Controls.Add(Me.matrixDataGrid)
+        Me.Name = "MatrixForm"
+        Me.Text = "MatrixForm"
+        CType(Me.matrixDataGrid, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents matrixDataGrid As System.Windows.Forms.DataGridView
+    Friend WithEvents apply_btn As System.Windows.Forms.Button
+    Friend WithEvents cancel_btn As System.Windows.Forms.Button
+End Class
---
+++ DistortionDetection/MatrixForm.vb
@@ -0,0 +1,55 @@
+﻿Imports ImageHandlerNamespace
+
+Public Class MatrixForm
+    Private _imageHandler As ImageHandler = Nothing
+    Private mask As Single(,)
+
+    Public Property ImHandler As ImageHandler
+        Get
+            Return _imageHandler
+        End Get
+        Set(value As ImageHandler)
+            _imageHandler = value
+        End Set
+    End Property
+
+    Private Function getMatrix() As Single(,)
+        If (matrixDataGrid.Rows.Count = 0 Or matrixDataGrid.Columns.Count = 0) Then Return Nothing
+
+        mask = New Single(matrixDataGrid.Rows.Count - 1, matrixDataGrid.Columns.Count - 1) {}
+        For r As Integer = 0 To mask.GetLength(0) - 1
+            For c As Integer = 0 To mask.GetLength(1) - 1
+                mask(r, c) = CSng(matrixDataGrid.Rows(r).Cells(c).Value)
+            Next c
+        Next r
+        Return mask
+    End Function
+
+    Private Sub apply_btn_Click(sender As Object, e As EventArgs) Handles apply_btn.Click
+        If IsNothing(_imageHandler) Then Return
+
+        ImHandler.prepareImage(ImageHandler.preparingMode.matrixFilt, getMatrix())
+
+    End Sub
+
+    Private Sub cancel_btn_Click(sender As Object, e As EventArgs) Handles cancel_btn.Click
+        Me.Hide()
+    End Sub
+
+    Private Sub matrixDataGrid_CellValidating(sender As Object, e As DataGridViewCellValidatingEventArgs) Handles matrixDataGrid.CellValidating
+
+        Me.matrixDataGrid.Rows(e.RowIndex).ErrorText = ""
+        Dim newSingle As Single
+
+        ' Don't try to validate the 'new row' until finished 
+        ' editing since there
+        ' is not any point in validating its initial value.
+        If matrixDataGrid.Rows(e.RowIndex).IsNewRow Then Return
+        If Not Single.TryParse(e.FormattedValue.ToString(), newSingle) Then
+            e.Cancel = True
+            Me.matrixDataGrid.Rows(e.RowIndex).ErrorText = "the value must be single"
+
+        End If
+
+    End Sub
+End Class
